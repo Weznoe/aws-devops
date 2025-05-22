3cf8827e4bd2f1d65ff4877b834fe881b8e6e06d
Weznoe <45051798+Weznoe@users.noreply.github.com>
2025-05-22 21:18:41 +0000

    add a cluster yaml

--- eks/deploy.sh
+++ eks/deploy.sh
@@ -1,1 +1,12 @@
 # Deploy an EKS cluster
+CLUSTER_NAME=${1:-'test-cluster'}
+REGION=$2
+
+sed -i "s/<initials-cluster>/$CLUSTER_NAME/" cluster.yaml
+
+if [ -n "$REGION" ]
+then
+    sed -i "s/us-east-1/$REGION/" cluster.yaml
+fi
+
+# eksctl create cluster -f cluster.yaml
