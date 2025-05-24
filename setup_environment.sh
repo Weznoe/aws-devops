6633b22bde74365eb5be3944406e62f3df0f3094
Weznoe <45051798+Weznoe@users.noreply.github.com>
2025-05-24 21:00:28 +0000

    make setup script executable

--- setup_environment.sh
+++ setup_environment.sh
@@ -8,6 +8,7 @@ sudo yum -y install terraform
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.2/2024-11-15/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p ~/bin && cp ./kubectl ~/bin/kubectl && export PATH=~/bin:$PATH
+rm ./kubectl
 
 # install eksctl
 export ARCH=amd64
